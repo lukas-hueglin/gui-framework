55a4590a43fcb6c82c6e1ffbce9fde131010f85a
lukas.hueglin <lukas.hueglin@outlook.com>
2024-09-04 17:37:54 +0200

    Added a math utilities namespace
    Added a namespace with helpful math objects and maybe functions in the future. Added a 2D point and a rectangle.

---
+++ GuiFramework/Include/Common/MathUtils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Math {
+
+	struct Point2D {
+		float x;
+		float y;
+	};
+
+	struct Rect {
+		float left;
+		float right;
+		float top;
+		float bottom;
+	};
+}
---
+++ GuiFramework/Source/Common/MathUtils.cpp
@@ -0,0 +1,2 @@
+#include "Gui.h"
+#include "Common/MathUtils.h"
